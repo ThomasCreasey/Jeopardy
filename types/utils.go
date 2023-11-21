85fa07fc6db84e64b91cbd75114db53189f9b181
Cryptonized <56192561+ItzCryptonized@users.noreply.github.com>
2023-11-21 10:31:26 +0000

    Add comments

--- types/utils.go
+++ types/utils.go
@@ -1,21 +1,19 @@
 package types
 
-type GetQuestionOutput struct {
-	Question string
-	Answer   string
-}
-
+// Structure that stores that current state of the question in the room
 type QuestionData struct {
 	Question string
 	Answers  []string
 	Answered bool
 }
 
+// Structure that represents a question with its value
 type ValueData struct {
 	Value    string
 	Question QuestionData
 }
 
+// Structure that represents a category with its values, each value also represents all its questions
 type CategoryData struct {
 	Category string
 	Values   []ValueData
--- utils/CheckQuestionsAnswered.go
+++ utils/CheckQuestionsAnswered.go
@@ -4,6 +4,8 @@ import (
 	"jeopardy/types"
 )
 
+// Checks to see if there are any questions in the category that are still waiting to be answered
+// Used to check if the game is over
 func CheckQuestionsAnswered(category []types.CategoryData) bool {
 	allAnswered := true
 
